8ce985940e51a74150d144e1a6fe22e11cde6ccd
Ben Wolfaardt <benjaminwolfaardt@gmail.com>
2024-02-27 15:23:34 +0100

    Add project directories

--- roles/zsh/templates/shortcuts.zsh
+++ roles/zsh/templates/shortcuts.zsh
@@ -1,2 +1,9 @@
 # Shortcuts to local projects
+ans=~/src/config/Ansible_Ubuntu_Dev_Config
+dot=~/src/config/dotfiles
 mvp=~/src/bhtechio/mvp
+
+# Shortcuts to local project directories
+bht=~/src/bhtechio/
+ion=~/src/ionsie/
+tut=~/src/tuts/
