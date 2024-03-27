f78f7feacc8ef795f15fc4390e97ae7cb957fba1
Ben Wolfaardt <benjaminwolfaardt@gmail.com>
2024-03-27 20:43:03 +0200

    Add zsh aliases

---
+++ roles/zsh/templates/aliases.zsh
@@ -0,0 +1,25 @@
+# Default oh my zsh
+
+# Docker
+alias d='docker'
+alias dc='docker compose'
+alias dcd='docker compose down'
+alias dcdd='docker compose down -d'
+alias dcu='docker compose up'
+alias dcub='docker compose up --build'
+alias dcud='docker compose up -d'
+alias de='docker exec -it'
+alias din='docker info'
+alias di='docker image'
+alias dil='docker image ls'
+alias dn='docker network'
+alias dni='docker network inspect'
+alias dnl='docker network ls'
+alias dp='docker ps'
+alias dv='docker volume'
+alias dvi='docker volume inspect'
+alias dvl='docker volume ls'
+
+# git
+# See also ~/src/config/dotfiles/.gitconfig
+alias g='git'
